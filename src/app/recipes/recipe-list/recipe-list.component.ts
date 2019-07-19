b8887909b099d46cc9fc5b8dc1a1903b567aa5b1
Carlos Dominguez <CarlosFernando.DominguezLara@windriver.com>
2019-07-19 11:14:35 -0600

    Adding Training Test

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is simply a test", "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
