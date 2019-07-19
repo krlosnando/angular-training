dab46ebfb96e1d4ba3f4e4a05de371c1cf57b51d
Carlos Dominguez <CarlosFernando.DominguezLara@windriver.com>
2019-07-19 11:38:15 -0600

    Adding Model changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is simply a test", "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg"),
     new Recipe("Test Recipe", "This is simply a test", "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg")
   ];
 
