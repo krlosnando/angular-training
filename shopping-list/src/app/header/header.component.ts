2a44820ced00782e26e65252a322904cdb257982
Carlos Dominguez <CarlosFernando.DominguezLara@windriver.com>
2019-07-22 13:43:05 -0600

    Changes

--- shopping-list/src/app/header/header.component.ts
+++ shopping-list/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+  }
 }
